4ab81345f9484e26c6c6d8ca0d973551c9fcb14b
BryceGetchell <bgetchell@umassd.edu>
2023-02-03 13:07:50 -0500

    CustomCarousel added for use in future

--- lib/CustomCarousel.dart
+++ lib/CustomCarousel.dart
@@ -1,89 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CustomCarousel extends StatefulWidget {
+  const CustomCarousel({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Brainstorm!'),
-    );
-  }
+  _CustomCarouselState createState() => _CustomCarouselState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _btnAction() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Files',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              IconButton(
-                icon: const Icon(Icons.person),
-                tooltip: 'Profile',
-                onPressed: () {
-                  // Add code to open profile page
-                },
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Search',
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _btnAction,
-          tooltip: 'New',
-          child: const Icon(Icons.add),
-        ));
-  }
-}
-
-class CustomCarouselFB2 extends StatefulWidget {
-  const CustomCarouselFB2({Key? key}) : super(key: key);
-
-  @override
-  _CustomCarouselFB2State createState() => _CustomCarouselFB2State();
-}
-
-class _CustomCarouselFB2State extends State<CustomCarouselFB2> {
+class _CustomCarouselState extends State<CustomCarousel> {
   // - - - - - - - - - - - - Instructions - - - - - - - - - - - - - -
   // 1.Replace cards list with whatever widgets you'd like.
   // 2.Change the widgetMargin attribute, to ensure good spacing on all screensize.
