284f39fc39b72690a1fb23d348119d2533058ffb
BryceGetchell <bgetchell@umassd.edu>
2023-02-06 14:04:34 -0500

    updated search bar and moved text

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 //import 'package:brainstorm/CustomCarousel.dart';
+import 'SearchInput.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,65 +12,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Brainstorm!'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _btnAction() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
+      home: Scaffold(
+        body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Text(
-                'Files',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              IconButton(
-                icon: const Icon(Icons.person),
-                tooltip: 'Profile',
-                onPressed: () {
-                  // Add code to open profile page
-                },
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Search',
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Files',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: IconButton(
+                      icon: const Icon(Icons.person),
+                      tooltip: 'Profile',
+                      onPressed: () {
+                        // Add code to open profile page
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SearchInput(
+                    textController: TextEditingController(),
+                    hintText: 'Search'),
               ),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: _btnAction,
+          onPressed: () {},
           tooltip: 'New',
           child: const Icon(Icons.add),
-        ));
+        ),
+      ),
+    );
   }
 }
