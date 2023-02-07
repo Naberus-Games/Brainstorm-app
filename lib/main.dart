7634d6b08685da31d3abaffa0dd45a47fc429aa8
BryceGetchell <bgetchell@umassd.edu>
2023-02-06 23:06:53 -0500

    Co-authored-by: Jonathen Colon <made10223@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'CustomCarousel.dart';
-import 'SearchInput.dart';
+// import 'widgets/CustomCarousel.dart';
+import 'widgets/SearchInput.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(12.0),
                     child: Text(
                       'Files',
                       style: Theme.of(context)
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(12.0),
                     child: Container(
                       width: 40,
                       height: 40,
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
                 ],
               ),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(12.0),
                 child: SearchInput(
                     textController: TextEditingController(),
                     hintText: 'Search'),
@@ -60,7 +60,7 @@ class MyApp extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(12.0),
                     child: Text(
                       'Recents',
                       style: Theme.of(context)
@@ -71,10 +71,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
-              // const Padding(
-              //   padding: EdgeInsets.all(8.0),
-              //   child: CustomCarousel(),
-              // ),
+              //const CustomCarousel(),
             ],
           ),
         ),
@@ -84,6 +81,149 @@ class MyApp extends StatelessWidget {
           tooltip: 'New',
           backgroundColor: const Color(0xff74D9DF),
           child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCarousel extends StatefulWidget {
+  const CustomCarousel({Key? key}) : super(key: key);
+
+  @override
+  _CustomCarouselState createState() => _CustomCarouselState();
+}
+
+class _CustomCarouselState extends State<CustomCarousel> {
+  // - - - - - - - - - - - - Instructions - - - - - - - - - - - - - -
+  // 1.Replace cards list with whatever widgets you'd like.
+  // 2.Change the widgetMargin attribute, to ensure good spacing on all screensize.
+  // 3.If you have a problem with this widget, please contact us at flutterbricks90@gmail.com
+  // Learn to build this widget at https://www.youtube.com/watch?v=dSMw1Nb0QVg!
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  List<Widget> cards = [
+    CardFb1(
+        text: "Explore",
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/illustrations%2Fundraw_Working_late_re_0c3y%201.png?alt=media&token=7b880917-2390-4043-88e5-5d58a9d70555",
+        subtitle: "+30 books",
+        onPressed: () {}),
+    CardFb1(
+        text: "Explore",
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/illustrations%2Fundraw_Designer_re_5v95%201.png?alt=media&token=5d053bd8-d0ea-4635-abb6-52d87539b7ec",
+        subtitle: "+30 books",
+        onPressed: () {}),
+    CardFb1(
+        text: "Explore",
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/illustrations%2Fundraw_Accept_terms_re_lj38%201.png?alt=media&token=476b97fd-ba66-4f62-94a7-bce4be794f36",
+        subtitle: "+30 books",
+        onPressed: () {})
+  ];
+
+  final double carouselItemMargin = 16;
+
+  late PageController _pageController;
+  int _position = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0, viewportFraction: .7);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+        controller: _pageController,
+        itemCount: cards.length,
+        onPageChanged: (int position) {
+          setState(() {
+            _position = position;
+          });
+        },
+        itemBuilder: (BuildContext context, int position) {
+          return imageSlider(position);
+        });
+  }
+
+  Widget imageSlider(int position) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, widget) {
+        return Container(
+          margin: EdgeInsets.all(carouselItemMargin),
+          child: Center(child: widget),
+        );
+      },
+      child: Container(
+        child: cards[position],
+      ),
+    );
+  }
+}
+
+class CardFb1 extends StatelessWidget {
+  final String text;
+  final String imageUrl;
+  final String subtitle;
+  final Function() onPressed;
+
+  const CardFb1(
+      {required this.text,
+      required this.imageUrl,
+      required this.subtitle,
+      required this.onPressed,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 250,
+        height: 230,
+        padding: const EdgeInsets.all(30.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.5),
+          boxShadow: [
+            BoxShadow(
+                offset: const Offset(10, 20),
+                blurRadius: 10,
+                spreadRadius: 0,
+                color: Colors.grey.withOpacity(.05)),
+          ],
+        ),
+        child: Column(
+          children: [
+            Image.network(imageUrl, height: 90, fit: BoxFit.cover),
+            const Spacer(),
+            Text(text,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                )),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              subtitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 12),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
         ),
       ),
     );
