7b7349065953fb3f2b66906dd26d9541721d7774
BryceGetchell <bgetchell@umassd.edu>
2023-02-07 00:08:48 -0500

    UPDATESSSS

--- lib/widgets/CustomCarousel.dart
+++ lib/widgets/CustomCarousel.dart
@@ -1,144 +1,83 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class CustomCarousel extends StatefulWidget {
-//   const CustomCarousel({Key? key}) : super(key: key);
+class CategoriesListMallika1 extends StatelessWidget {
+  const CategoriesListMallika1({Key? key}) : super(key: key);
 
-//   @override
-//   _CustomCarouselState createState() => _CustomCarouselState();
-// }
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          CategoryCardMallika1(
+            title: "Cake",
+            image:
+                "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/malika%2FImage-1.png?alt=media&token=245741de-7966-4f5b-805f-6dd8e5dbea80",
+            onTap: () {},
+            selected: false,
+          ),
+          CategoryCardMallika1(
+              title: "Pasta",
+              image:
+                  "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/malika%2FImage-2.png?alt=media&token=3ae6f4e6-53cd-4ac1-9e12-b491c351771a",
+              onTap: () {}),
+          CategoryCardMallika1(
+              title: "Drinks",
+              image:
+                  "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/malika%2FImage-3.png?alt=media&token=43ed89ab-93de-42ca-90df-0231699eebf0",
+              onTap: () {}),
+          CategoryCardMallika1(
+              title: "Rice",
+              image:
+                  "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/malika%2FImage-4.png?alt=media&token=0c2592ca-7bbc-40fa-80ae-163d739e308f",
+              onTap: () {}),
+        ],
+      ),
+    );
+  }
+}
 
-// class _CustomCarouselState extends State<CustomCarousel> {
-//   // - - - - - - - - - - - - Instructions - - - - - - - - - - - - - -
-//   // 1.Replace cards list with whatever widgets you'd like.
-//   // 2.Change the widgetMargin attribute, to ensure good spacing on all screensize.
-//   // 3.If you have a problem with this widget, please contact us at flutterbricks90@gmail.com
-//   // Learn to build this widget at https://www.youtube.com/watch?v=dSMw1Nb0QVg!
-//   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+class CategoryCardMallika1 extends StatelessWidget {
+  final String title;
+  final String image;
+  final Function() onTap;
+  final bool selected;
+  const CategoryCardMallika1(
+      {required this.title,
+      required this.image,
+      required this.onTap,
+      this.selected = false,
+      Key? key})
+      : super(key: key);
 
-//   List<Widget> cards = [
-//     CardFb1(
-//         text: "Explore",
-//         imageUrl:
-//             "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/illustrations%2Fundraw_Working_late_re_0c3y%201.png?alt=media&token=7b880917-2390-4043-88e5-5d58a9d70555",
-//         subtitle: "+30 books",
-//         onPressed: () {}),
-//     CardFb1(
-//         text: "Explore",
-//         imageUrl:
-//             "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/illustrations%2Fundraw_Designer_re_5v95%201.png?alt=media&token=5d053bd8-d0ea-4635-abb6-52d87539b7ec",
-//         subtitle: "+30 books",
-//         onPressed: () {}),
-//     CardFb1(
-//         text: "Explore",
-//         imageUrl:
-//             "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/illustrations%2Fundraw_Accept_terms_re_lj38%201.png?alt=media&token=476b97fd-ba66-4f62-94a7-bce4be794f36",
-//         subtitle: "+30 books",
-//         onPressed: () {})
-//   ];
-
-//   final double carouselItemMargin = 16;
-
-//   late PageController _pageController;
-//   int _position = 0;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _pageController = PageController(initialPage: 0, viewportFraction: .7);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return PageView.builder(
-//         controller: _pageController,
-//         itemCount: cards.length,
-//         onPageChanged: (int position) {
-//           setState(() {
-//             _position = position;
-//           });
-//         },
-//         itemBuilder: (BuildContext context, int position) {
-//           return imageSlider(position);
-//         });
-//   }
-
-//   Widget imageSlider(int position) {
-//     return AnimatedBuilder(
-//       animation: _pageController,
-//       builder: (BuildContext context, widget) {
-//         return Container(
-//           margin: EdgeInsets.all(carouselItemMargin),
-//           child: Center(child: widget),
-//         );
-//       },
-//       child: Container(
-//         child: cards[position],
-//       ),
-//     );
-//   }
-// }
-
-// class CardFb1 extends StatelessWidget {
-//   final String text;
-//   final String imageUrl;
-//   final String subtitle;
-//   final Function() onPressed;
-
-//   const CardFb1(
-//       {required this.text,
-//       required this.imageUrl,
-//       required this.subtitle,
-//       required this.onPressed,
-//       Key? key})
-//       : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: onPressed,
-//       child: Container(
-//         width: 250,
-//         height: 230,
-//         padding: const EdgeInsets.all(30.0),
-//         decoration: BoxDecoration(
-//           color: Colors.white,
-//           borderRadius: BorderRadius.circular(12.5),
-//           boxShadow: [
-//             BoxShadow(
-//                 offset: const Offset(10, 20),
-//                 blurRadius: 10,
-//                 spreadRadius: 0,
-//                 color: Colors.grey.withOpacity(.05)),
-//           ],
-//         ),
-//         child: Column(
-//           children: [
-//             Image.network(imageUrl, height: 90, fit: BoxFit.cover),
-//             const Spacer(),
-//             Text(text,
-//                 textAlign: TextAlign.center,
-//                 style: const TextStyle(
-//                   color: Colors.black,
-//                   fontWeight: FontWeight.bold,
-//                   fontSize: 18,
-//                 )),
-//             const SizedBox(
-//               height: 5,
-//             ),
-//             Text(
-//               subtitle,
-//               textAlign: TextAlign.center,
-//               style: const TextStyle(
-//                   color: Colors.grey,
-//                   fontWeight: FontWeight.normal,
-//                   fontSize: 12),
-//             ),
-//             const SizedBox(
-//               height: 10,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.network(
+                image,
+                width: 275,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 12.5,
+            ),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
