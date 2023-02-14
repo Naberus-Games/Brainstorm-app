2ef757e0fd39141d77657f44ad73312a2774ea9e
BryceGetchell <bgetchell@umassd.edu>
2023-02-14 01:32:01 -0500

    changed name of custom buttons

--- lib/widgets/categories_button.dart
+++ lib/widgets/categories_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color themeColor = Color(0xff74D9DF);
 
-Widget makeButton(IconData icon, String text) {
+Widget customBtn(IconData icon, String text) {
   return Padding(
     padding:
         const EdgeInsets.only(left: 20.0, right: 20.0, top: 4.0, bottom: 4.0),
