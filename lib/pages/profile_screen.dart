f8ea162d535fc84de00ffc9fd3b8fd3d5a1619f6
BryceGetchell <bgetchell@umassd.edu>
2023-02-17 13:28:12 -0500

    add user icon to settings page

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:brainstorm/widgets/custom_headings.dart';
 import 'package:brainstorm/widgets/entry_box.dart';
+import 'package:brainstorm/widgets/custom_bar_button.dart';
 
 import 'package:flutter/material.dart';
 
@@ -18,37 +19,37 @@ class ProfileScreen extends StatelessWidget {
         },
         child: Scaffold(
           body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const <Widget>[
-                MainHeading(text: "Profile"),
-                //need to add profile icon here
-
-                // Container(
-                //   padding: EdgeInsets.all(20),
-                //   child: CircleAvatar(
-                //     backgroundImage: AssetImage('assets/profile_icon.jpg'),
-                //     radius: 70,
-                //   ),
-                // ),
-
-                //cant get this to work :(
-
-                CustomEntry(hintText: "First Name"),
-                CustomEntry(hintText: "Last Name"),
-                CustomEntry(hintText: "User Name"),
-                CustomEntry(hintText: "Email"),
-                SubHeading(text: "Security"),
-                CustomEntry(hintText: "Current Password"),
-                CustomEntry(hintText: "New Password"),
-                CustomEntry(hintText: "Confirm Password"),
-                // BarButton(
-                //   text: "Update",
-                //   onPressed: () {},
-                // ),
-                //wont let you put button unless you add action, dont know
-                //how to get around this for now
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const <Widget>[
+                  MainHeading(text: "Profile"),
+                  //start profile icon
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 100,
+                        child:
+                            Icon(Icons.person, size: 150.0, color: themeColor)),
+                  ),
+                  //end profile icon
+                  CustomEntry(hintText: "First Name"),
+                  CustomEntry(hintText: "Last Name"),
+                  CustomEntry(hintText: "User Name"),
+                  CustomEntry(hintText: "Email"),
+                  SubHeading(text: "Security"),
+                  CustomEntry(hintText: "Current Password"),
+                  CustomEntry(hintText: "New Password"),
+                  CustomEntry(hintText: "Confirm Password"),
+                  BarButton(
+                    text: "Update",
+                    onPressed: () {},
+                  ),
+                  //wont let you put button unless you add action, dont know
+                  //how to get around this for now
+                ],
+              ),
             ),
           ),
           backgroundColor: const Color(0xff292828),
