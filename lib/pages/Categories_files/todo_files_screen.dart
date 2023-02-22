32e9a7262e7b17c8e34df2ea95b7f797bcc80777
BryceGetchell <bgetchell@umassd.edu>
2023-02-21 23:06:24 -0500

    homeowrk assignment

--- lib/pages/Categories_files/todo_files_screen.dart
+++ lib/pages/Categories_files/todo_files_screen.dart
@@ -1,35 +1,120 @@
+import 'package:brainstorm/main.dart';
 import 'package:flutter/material.dart';
+import '../../theme.dart';
+import '../../widgets/categories_button.dart';
+import '../../widgets/custom_headings.dart';
 
-const MaterialColor themeColor = Colors.lightBlue;
+const leftVal = 20.0, rightVal = 20.0, topVal = 4.0, bottomVal = 4.0;
+const radialVal = 30.0;
+const maxItems = SnackBar(
+  content: Text('Max Items Reached'),
+);
+
+List<String> todoList = ["fisrt", "next", "another one", "again"];
+const int listAmount = 1;
 
 class ToDoFilesScreen extends StatelessWidget {
   const ToDoFilesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: SafeArea(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'To Do',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium
-                            ?.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        // ignore: sort_child_properties_last
+        child: Scaffold(
+          body: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                floating: false,
+                pinned: false,
+                automaticallyImplyLeading: false,
+                actions: [
+                  //Icon Button
+                  const Text('Delete All'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.white,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.delete,
+                            color: const BrainstormTheme().primaryColor,
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+              SliverAppBar(
+                floating: true,
+                pinned: true,
+                automaticallyImplyLeading: false,
+                flexibleSpace: const FlexibleSpaceBar(
+                  titlePadding: EdgeInsets.fromLTRB(12, 0, 0, 100),
+                  title: Text(
+                    "To Do",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  centerTitle: false,
+                ),
+                bottom: PreferredSize(
+                  preferredSize: const Size(0, 90),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: leftVal, right: rightVal, top: 0, bottom: 12),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: context.isDarkMode
+                            ? const BrainstormTheme().darkAccentColor
+                            : const BrainstormTheme().lightAccentColor,
+                        borderRadius: BorderRadius.circular(radialVal),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(fontSize: 17),
+                          hintText: 'Enter index to delete',
+                          prefixIcon: Icon(Icons.local_grocery_store),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(20),
+                        ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ]),
+              ),
+              SliverToBoxAdapter(
+                child: filledUpdateBtn('Delete Index', null, context),
+              ),
+              const SliverToBoxAdapter(
+                child: SubHeading(text: '               My Shopping List'),
+              ),
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                Column(
+                  children: [
+                    for (int i = 0; i < todoList.length; i++)
+                      Text('${i + 1}. ${todoList[i]}')
+                  ],
+                )
+              ])),
+              const SliverToBoxAdapter(
+                  child: Padding(padding: EdgeInsets.all(15), child: Text(''))),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(
+              Icons.add,
+              color: const BrainstormTheme().lightAccentColor,
+            ),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(maxItems);
+            },
+          ),
         ),
-        backgroundColor: const Color(0xff292828),
       );
 }
--- lib/pages/Work_Screens/todo_work_screen.dart
+++ lib/pages/Work_Screens/todo_work_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:brainstorm/widgets/categories_button.dart';
+import 'package:brainstorm/widgets/custom_headings.dart';
 
-const MaterialColor themeColor = Colors.lightBlue;
+import '../../widgets/entry_box.dart';
 
 class ToDoWorkScreen extends StatelessWidget {
   const ToDoWorkScreen({super.key});
@@ -9,24 +11,37 @@ class ToDoWorkScreen extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
         body: SafeArea(
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'To Do',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium
-                            ?.copyWith(fontWeight: FontWeight.bold),
-                      ),
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'To Do',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium
+                          ?.copyWith(fontWeight: FontWeight.bold),
                     ),
-                  ],
-                ),
-              ]),
+                  ),
+                ],
+              ),
+              const MainHeading(text: '       CIS443 Shopping List'),
+              customTextField(
+                  "Enter your item to add", Icons.shopping_bag, false, context),
+              customBtn(Icons.add, "Add Your Item"),
+              const SubHeading(
+                  text: "Click items below to add\na frequent item"),
+              customBtn(Icons.apple, "Apple"),
+              customBtn(Icons.local_drink, "Milk"),
+              customBtn(Icons.rectangle, "Bread"),
+              customBtn(Icons.smartphone, "Phone Case"),
+              customBtn(Icons.mouse, "Mouse"),
+              filledUpdateBtn('Update', null, context),
+            ],
+          ),
         ),
       );
 }
