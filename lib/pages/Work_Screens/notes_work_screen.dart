e2c54c7ac337db67b67fbf0ddd197e9c430b9dcb
BryceGetchell <bgetchell@umassd.edu>
2023-03-05 01:01:45 -0500

    updates to notes sectoin

--- lib/pages/Work_Screens/notes_work_screen.dart
+++ lib/pages/Work_Screens/notes_work_screen.dart
@@ -1,245 +1,225 @@
-// ignore_for_file: unused_import
+// ignore_for_file: library_private_types_in_public_api, camel_case_types
 
-import 'package:brainstorm/main.dart';
-import 'package:brainstorm/theme.dart';
-import 'package:brainstorm/widgets/categories_button.dart';
-import 'package:brainstorm/widgets/custom_headings.dart';
-import '../../widgets/recents.dart';
 import 'package:flutter/material.dart';
-
-const leftVal = 20.0, rightVal = 20.0, topVal = 4.0, bottomVal = 4.0;
-const radialVal = 30.0;
 
 class NotesWorkScreen extends StatelessWidget {
   const NotesWorkScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => GestureDetector(
-        onTap: () {
-          FocusScopeNode currentFocus = FocusScope.of(context);
-          if (!currentFocus.hasPrimaryFocus) {
-            currentFocus.unfocus();
-          }
-        },
-        // ignore: sort_child_properties_last
-        child: Scaffold(
-          body: CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                floating: false,
-                pinned: false,
-                automaticallyImplyLeading: false,
-                actions: [
-                  //Popup Buttons
-                  Padding(
-                    padding: const EdgeInsets.all(9.5),
-                    child: PopupMenuButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
-                      icon: Icon(
-                        Icons.account_circle,
-                        color: const BrainstormTheme().primaryColor,
-                        size: 40,
-                      ),
-                      color: context.isDarkMode
-                          ? const BrainstormTheme().darkAccentColor
-                          : const BrainstormTheme().lightAccentColor,
-                      onSelected: (value) {
-                        switch (value) {
-                          case 1:
-                            Navigator.pushNamed(context, '/Profile-Screen');
-                            break;
-                          case 2:
-                            Navigator.pushNamed(context, '/Settings-Screen');
-                            break;
-                          case 3:
-                            Navigator.pushNamedAndRemoveUntil(
-                                context, '/', (route) => false);
-                            break;
-                        }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: const <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  hintText: 'Enter a title',
+                  hintStyle: TextStyle(fontSize: 20)),
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+            TextField(
+              maxLines: 24,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                hintText: 'Write a note',
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 50.0,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const bottomSheetContent();
                       },
-                      itemBuilder: (context) => [
-                        PopupMenuItem(
-                          value: 1,
-                          child: ListTile(
-                            leading: Icon(
-                              Icons.person,
-                              color: const BrainstormTheme().primaryColor,
-                            ),
-                            title: Text('Profile',
-                                style: Theme.of(context).textTheme.bodyMedium),
-                          ),
-                        ),
-                        PopupMenuItem(
-                          value: 2,
-                          child: ListTile(
-                            leading: Icon(
-                              Icons.settings,
-                              color: const BrainstormTheme().primaryColor,
-                            ),
-                            title: Text('Setting',
-                                style: Theme.of(context).textTheme.bodyMedium),
-                          ),
-                        ),
-                        PopupMenuItem(
-                          value: 3,
-                          child: ListTile(
-                            leading: const Icon(
-                              Icons.logout,
-                              color: Colors.red,
-                            ),
-                            title: Text(
-                              'Logout',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(color: Colors.red),
-                            ),
-                          ),
-                        ),
-                      ],
-                      tooltip: 'Profile',
-                    ),
-                  ),
-                ],
-              ),
-              SliverAppBar(
-                floating: true,
-                pinned: true,
-                automaticallyImplyLeading: false,
-                flexibleSpace: const FlexibleSpaceBar(
-                  titlePadding: EdgeInsets.fromLTRB(12, 0, 0, 100),
-                  title: Text(
-                    "Files",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                  ),
-                  centerTitle: false,
-                ),
-                bottom: PreferredSize(
-                  preferredSize: const Size(0, 90),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: leftVal, right: rightVal, top: 0, bottom: 12),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: context.isDarkMode
-                            ? const BrainstormTheme().darkAccentColor
-                            : const BrainstormTheme().lightAccentColor,
-                        borderRadius: BorderRadius.circular(radialVal),
-                      ),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          hintStyle: TextStyle(fontSize: 17),
-                          hintText: 'Search',
-                          prefixIcon: Icon(Icons.search),
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.all(20),
-                        ),
-                      ),
-                    ),
-                  ),
+                    );
+                  },
+                  icon: const Icon(Icons.text_format),
+                  label: const Text('Format'),
                 ),
               ),
-              const SliverToBoxAdapter(
-                child: Recents(),
+              const Expanded(
+                child: Text(''),
               ),
-              const SliverToBoxAdapter(
-                child: Recents(),
-              ),
-              const SliverToBoxAdapter(
-                child: Recents(),
-              ),
-              const SliverToBoxAdapter(
-                child: Recents(),
-              ),
-              const SliverToBoxAdapter(
-                child: Recents(),
-              ),
-              const SliverToBoxAdapter(
-                child: Recents(),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Save'),
+                ),
               ),
             ],
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: PopupMenuButton(
-              offset: Offset.fromDirection(0, 100),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0)),
-              icon: const Icon(
-                Icons.add,
-                color: Colors.white,
-              ),
-              color: context.isDarkMode
-                  ? const BrainstormTheme().darkAccentColor
-                  : const BrainstormTheme().lightAccentColor,
-              onSelected: (value) {
-                switch (value) {
-                  case 1:
-                    Navigator.pushNamed(context, '/Profile-Screen');
-                    break;
-                  case 2:
-                    Navigator.pushNamed(context, '/Settings-Screen');
-                    break;
-                  case 3:
-                    Navigator.pushNamed(context, '/');
-                    break;
-                  case 4:
-                    Navigator.pushNamed(context, '/');
-                    break;
-                }
-              },
-              itemBuilder: (context) => [
-                PopupMenuItem(
-                  value: 1,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.crop_original,
-                      color: const BrainstormTheme().primaryColor,
-                    ),
-                    title: Text('Whiteboard',
-                        style: Theme.of(context).textTheme.bodyMedium),
-                  ),
-                ),
-                PopupMenuItem(
-                  value: 2,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.notes,
-                      color: const BrainstormTheme().primaryColor,
-                    ),
-                    title: Text('Notes',
-                        style: Theme.of(context).textTheme.bodyMedium),
-                  ),
-                ),
-                PopupMenuItem(
-                  value: 3,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.check_box,
-                      color: const BrainstormTheme().primaryColor,
-                    ),
-                    title: Text(
-                      'Todo',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ),
-                ),
-                PopupMenuItem(
-                  value: 4,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.edit_document,
-                      color: const BrainstormTheme().primaryColor,
-                    ),
-                    title: Text('Scripts',
-                        style: Theme.of(context).textTheme.bodyMedium),
-                  ),
-                ),
-              ],
-              tooltip: 'New',
+        ),
+      ),
+    );
+  }
+}
+
+class bottomSheetContent extends StatefulWidget {
+  const bottomSheetContent({super.key});
+
+  @override
+  _bottomSheetContentState createState() => _bottomSheetContentState();
+}
+
+class _bottomSheetContentState extends State<bottomSheetContent> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          title: Text(
+            'Options',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
-      );
+        ListTile(
+          leading: Icon(Icons.format_bold),
+          title: Text('Bold'),
+        ),
+        ListTile(
+          leading: Icon(Icons.format_italic),
+          title: Text('Italic'),
+        ),
+        ListTile(
+          leading: Icon(Icons.format_underlined),
+          title: Text('Underline'),
+        ),
+        ListTile(
+          leading: Icon(Icons.strikethrough_s),
+          title: Text('Strikethrough'),
+        ),
+        ListTile(
+          leading: Icon(Icons.text_format_sharp),
+          title: Text('Superscript'),
+        ),
+        ListTile(
+          leading: Icon(Icons.text_format_outlined),
+          title: Text('Subscript'),
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.format_size),
+          title: Text('Paragraph Styles'),
+        ),
+        ListTile(
+          leading: Icon(Icons.format_align_left),
+          title: Text('Align'),
+        ),
+        ListTile(
+          onTap: () => lineSpacingMenu(),
+          leading: Icon(Icons.format_line_spacing),
+          title: Text('Line spacing'),
+        ),
+        ListTile(
+          leading: Icon(Icons.format_list_numbered),
+          title: Text('Numbered lists'),
+        ),
+        ListTile(
+          leading: Icon(Icons.format_list_bulleted),
+          title: Text('List options'),
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.format_clear),
+          title: Text('Clear formatting'),
+        ),
+      ],
+    );
+  }
+
+  lineSpacingMenu() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        double? _lineSpacing = 1.0;
+        var _customLineSpacing = 1.0;
+        return AlertDialog(
+          title: const Text('Line Spacing'),
+          content: Container(
+            height: 230.0,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: const Text('Single'),
+                  leading: Radio(
+                    value: 1.0,
+                    groupValue: _lineSpacing,
+                    onChanged: (value) {
+                      setState(() {
+                        _lineSpacing = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('1.15'),
+                  leading: Radio(
+                    value: 1.15,
+                    groupValue: _lineSpacing,
+                    onChanged: (value) {
+                      setState(() {
+                        _lineSpacing = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Double'),
+                  leading: Radio(
+                    value: 2.0,
+                    groupValue: _lineSpacing,
+                    onChanged: (value) {
+                      setState(() {
+                        _lineSpacing = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Custom...'),
+                  leading: Radio(
+                    value: _customLineSpacing,
+                    groupValue: _lineSpacing,
+                    onChanged: (value) {
+                      setState(() {
+                        _lineSpacing = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            MaterialButton(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            MaterialButton(
+              child: const Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
